846f7be212eaf627016e3d8998bcf590889c7106
nitishyadav23 <nitishkr181006@gmail.com>
2025-08-25 15:46:08 +0530

    add

---
+++ nitish/config/dbConfig.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const dbConnect = async ()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Database connected successfully");
+       
+    }
+    catch(error){
+        console.error("Database connection failed:", error);
+        process.exit(1); 
+    }
+}
+export default dbConnect;
